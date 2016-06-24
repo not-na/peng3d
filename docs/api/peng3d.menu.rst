6826e2b8bd438de7066e380603de398a8b43d5d4
not-na <notna@apparat.org>
2016-06-24 18:05:38 +0200

    Added docs for peng3d.menu
    - Added docs for peng3d.menu
    - Added safety check for Menu.addLayer
    - Added pip/easy_install via PyPI reference to README.md

---
+++ docs/api/peng3d.menu.rst
@@ -0,0 +1,7 @@
+
+``peng3d.menu`` - Flexible menu system
+======================================
+
+.. automodule:: peng3d.menu
+   :members:
+   :synopsis: Flexible menu system
--- peng3d/menu.py
+++ peng3d/menu.py
@@ -22,30 +22,73 @@
 #  
 #  
 
+from .layer import Layer
+
 class BasicMenu(object):
+    """
+    Menu base class without layer support.
+    
+    Each menu is separated from the other menus and can be switched between at any time.
+    
+    .. seealso::
+       
+       See :py:class:`Menu()` for more information.
+    
+    """
+    
     def __init__(self,name,window,peng):
         self.name = name
         self.window = window
         self.peng = peng
     def draw(self):
+        """
+        This method is called if it is time to render the menu.
+        
+        Override this method in subclasses to cutomize behaviour and actually draw stuff.
+        """
         pass
     
     # Event handlers
     def on_enter(self,old):
+        """
+        This fake event handler will be called everytime this menu is entered via the :py:meth:`PengWindow.changeMenu()`\ .
+        
+        This handler will not be called if this menu is already active.
+        """
         pass # Custom fake event handler for entering the menu
 
 class Menu(BasicMenu):
+    """
+    Subclass of :py:class:`BasicMenu` adding support for the :py:class:`Layer` class.
+    
+    This subclass overrides the draw and __init__ method, so be sure to call them if you override them.
+    """
+    
     def __init__(self,name,window,peng):
         super(Menu,self).__init__(name,window,peng)
         self.layers = []
     def addLayer(self,layer,z=-1):
+        """
+        Adds a new layer to the stack, optionally at the specified z-value.
+        
+        ``layer`` must be an instance of Layer or subclasses.
+        
+        ``z`` can be used to override the index of the layer in the stack. Defaults to ``-1`` for appending.
+        """
         # Adds a new layer to the stack, optionally at the specified z-value
         # The z-value is the index this layer should be inserted in, or -1 for appending
+        if not isinstance(layer,Layer):
+            raise TypeError("layer must be an instance of Layer!")
         if z==-1:
             self.layers.append(layer)
         else:
             self.layers.insert(z,layer)
     def draw(self):
+        """
+        Draws the layers in the appropriate order.
+        
+        Layers that have their ``enabled`` attribute set to ``False`` are skipped.
+        """
         # Draws each layer in the given order
         for layer in self.layers:
             if layer.enabled:
